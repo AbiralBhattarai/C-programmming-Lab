00f75216021c66870b779d927c602361d2a017f1
Abiral <abiralbhattarai101@gmail.com>
2021-04-27 20:05:46 +0545

    master

---
+++ lab2/qn3b.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int a;
+	printf("enter a number: ");
+	scanf("%d",&a);
+	if(a/2 == 0){
+		printf("Number is even!");
+	}else if(a/2 == 1){
+		printf("Number is odd!");
+	}
+	else{
+		printf("number is zero!");
+	}
+}
