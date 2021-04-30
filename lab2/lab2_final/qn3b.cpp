d7c61892f39da891dfc7bd8b5d8f23645a6fde38
Abiral <abiralbhattarai101@gmail.com>
2021-04-30 22:23:00 +0545

    updated code

--- lab2/lab2_final/qn3b.cpp
+++ lab2/lab2_final/qn3b.cpp
@@ -1,9 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
 int main(){
-	int a;
-	printf("enter a number;");
+	int a,rem;
+	printf("enter a number:");
 	scanf("%d",&a);
-	if(){
-		printf("number is even!");
-	}else{
-		printf("number is odd");
+	while(a>=2){
+		a = a-2;
 	}
+	rem = a;
+	{(rem==0)?printf("even"):printf("odd");}
+}
